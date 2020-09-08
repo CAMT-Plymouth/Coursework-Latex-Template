8b7c822238bb066e6f64dc2c9d71bb5d6df0cad2
Dave Moffat <me@davemoffat.com>
2020-09-08 14:41:49 +0100

    creation of repository, with V1 of coursework latex template

---
+++ codeExample.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is a listing imported from an external file" << std::endl;
+    std::cin.get();
+    return 0;
+}
